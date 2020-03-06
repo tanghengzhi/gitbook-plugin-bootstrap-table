844abcdb3b6503066806b4bf4e088de79b27db1d
Victor Tang <tanghengzhi@live.com>
2020-03-06 23:20:20 +0800

    add treegrid support

--- assets/plugin.js
+++ assets/plugin.js
@@ -7,4 +7,25 @@ gitbook.events.on('page.change', function() {
 		showColumns: true,
 		showToggle: true
 	})
+
+	/**
+	 * treegrid
+	 */
+	$("table").find('tbody').find('tr').each(function(id) {
+		$(this).addClass('treegrid-' + id);
+		$(this).data('treegrid', id);
+
+		let match = $(this).html().match(/\[&gt;\]/g);
+		let level = match ? match.length : 0;
+
+		$(this).addClass('treegrid-level-' + level);
+		$(this).html($(this).html().replace(/\[&gt;\]/g, ""));
+
+		if (level > 0) {
+			let parent = $('.treegrid-level-' + (level - 1)).last();
+			$(this).addClass('treegrid-parent-' + parent.data('treegrid'));
+		}
+	})
+
+  $("table").treegrid();
 });
--- index.js
+++ index.js
@@ -7,12 +7,15 @@ module.exports = {
 				'https://unpkg.com/bootstrap-table@1.16.0/dist/bootstrap-table.min.js',
 				'https://unpkg.com/jquery-resizable-columns@0.2.3/dist/jquery.resizableColumns.min.js',
 				'https://unpkg.com/bootstrap-table@1.16.0/dist/extensions/resizable/bootstrap-table-resizable.min.js',
+				'https://cdnjs.cloudflare.com/ajax/libs/jquery-treegrid/0.2.0/js/jquery.treegrid.min.js',
+				'https://unpkg.com/bootstrap-table@1.16.0/dist/extensions/treegrid/bootstrap-table-treegrid.min.js',
 				'plugin.js'
 			],
 			css: [
 				'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',
 				'https://unpkg.com/bootstrap-table@1.16.0/dist/bootstrap-table.min.css',
 				'https://unpkg.com/jquery-resizable-columns@0.2.3/dist/jquery.resizableColumns.css',
+				'https://cdnjs.cloudflare.com/ajax/libs/jquery-treegrid/0.2.0/css/jquery.treegrid.min.css',
 				'plugin.css'
 			]
     }
